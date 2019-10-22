50f6450868b00ae23bfc1ce60ebecd17aa431586
JuanCarlosgg <juancarlos97gg@gmail.com>
2019-10-22 11:34:16 +0200

    Pruebas

--- action-snips-skill-noticias.py
+++ action-snips-skill-noticias.py
@@ -44,22 +44,22 @@ def extraer_noticia():
 def intentHandler(hermes, intent_message, mensaje):
     #intent_stop(hermes,intent_message)
     if intent_message.intent.intent_name == 'jaimevegas:DiNoticias':
-        hermes.publish_continue_session(intent_message.session_id,  'Éstas son las noticias de hoy: '+  mensaje[1], ["juancarlos:Cancelar"])
-        """contenido =
+        hermes.publish_continue_session(intent_message.session_id,  'Éstas son las noticias de hoy: ', ["juancarlos:Cancelar"])
+        contenido = mensaje[1]
         for i in len(contenido):
-            hermes.publish_continue_session(intent_message.session_id, mensaje[1][i])
+            hermes.publish_continue_session(intent_message.session_id, mensaje[1][i], ["juancarlos:Cancelar"])
             intent_stop(hermes,intent_message)
-        """
+        
     elif intent_message.intent.intent_name == 'jaimevegas:DiTitulares':
-        hermes.publish_continue_session(intent_message.session_id,  'Éstos son los titulares de hoy: ' + mensaje[0], ["juancarlos:Cancelar"])
-        """titulares =  mensaje[0]
+        hermes.publish_continue_session(intent_message.session_id,  'Éstos son los titulares de hoy: ', ["juancarlos:Cancelar"])
+        titulares =  mensaje[0]
         for i in len(titulares):
-            hermes.publish_continue_session(intent_message.session_id, mensaje[1][i])
+            hermes.publish_continue_session(intent_message.session_id, mensaje[1][i], ["juancarlos:Cancelar"])
             intent_stop(hermes,intent_message)
-        """
+        
     else:
         return 
-    hermes.publish_end_session(intent_message.session_id, 'Ya me callo')
+    hermes.publish_end_session(intent_message.session_id, '')
 
     # hermes.publish_end_session(intent_message.session_id, sentence)                  
 def intent_received(hermes, intent_message):
@@ -70,7 +70,10 @@ def intent_received(hermes, intent_message):
 def intent_stop(hermes, intent_message):
     #if intent_message.intent.intent_name == 'juancarlos:Cancelar':
     hermes.publish_end_session(intent_message.session_id, 'Ya me callo')
-    
+
+def nextLine(hermes, intent_message, lines, i):
+    hermes.publish_continue_session(intent_message.session_id.id, lines, ["juancarlos:nextLine", "juancarlos:Cancelar"])    
+
 with Hermes(MQTT_ADDR) as h:
     #dialogue_conf = DialogueConfiguration().enable_intent("Stop")  
     
@@ -78,4 +81,5 @@ with Hermes(MQTT_ADDR) as h:
     h.subscribe_intent("jaimevegas:DiTitulares", intent_received) \
         .subscribe_intent("jaimevegas:DiNoticias", intent_received) \
         .subscribe_intent("juancarlos:Cancelar", intent_stop) \
+        .subscribe_intent("juancarlos:NextLine", nextLine) \
         .start()
