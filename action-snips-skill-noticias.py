b6d9d75335f5e85477997d424adb9f57e1e44fe0
JuanCarlosgg <juancarlos97gg@gmail.com>
2019-10-22 10:57:22 +0200

    Pruebas

--- action-snips-skill-noticias.py
+++ action-snips-skill-noticias.py
@@ -13,8 +13,6 @@ def extraer_noticia():
     noticias = ""
     cabeceras = ""
     url = "http://ep00.epimg.net/rss/elpais/portada.xml"
-    titulares = list()
-    contenidos = list()
     response = requests.get(url)
     webContent = response.text.encode('utf-8')
     webContent = webContent.decode()
@@ -37,23 +35,40 @@ def extraer_noticia():
         noticias += tit[1] + ".\r\n" + tit[3] + ".\r\n"
         result = [cabeceras, noticias]
     return result
+    
+#Poner un índice en el que se comprueba linea a linea si hay un stop.
+# https://docs.snips.ai/articles/platform/dialog/frame-based-dm
+# https://github.com/snipsco/hermes-protocol/tree/develop/platforms/hermes-javascript#api
+# https://github.com/MrJohnZoidberg/Snips-Einkaufsliste/blob/master/action-ShoppingList.py
+#
+def intentHandler(hermes, intent_message, mensaje):
+    intent_stop(hermes,intent_message)
+    if intent_message.intent.intent_name == 'jaimevegas:DiNoticias':
+        hermes.publish_continue_session(intent_message.session_id,  'Éstas son las noticias de hoy: ')
+        contenido = mensaje[1]
+        for i in len(contenido):
+            hermes.publish_continue_session(intent_message.session_id, mensaje[1][i])
+            intent_stop(hermes,intent_message)
 
+    elif intent_message.intent.intent_name == 'jaimevegas:DiTitulares':
+        hermes.publish_continue_session(intent_message.session_id,  'Éstos son los titulares de hoy: ')
+        titulares =  mensaje[0]
+        for i in len(titulares):
+            hermes.publish_continue_session(intent_message.session_id, mensaje[1][i])
+            intent_stop(hermes,intent_message)
+    else:
+        return 
+    hermes.publish_end_session(intent_message.session_id, 'Ya me callo')
+
+    # hermes.publish_end_session(intent_message.session_id, sentence)                  
 def intent_received(hermes, intent_message):
     mensaje = extraer_noticia()
-    
-    if intent_message.intent.intent_name == 'jaimevegas:DiNoticias':
-        sentence = 'Éstas son las noticias de hoy: ' + mensaje[1]
-    elif intent_message.intent.intent_name == 'jaimevegas:DiTitulares':
-        sentence = 'Éstos son los titulares de hoy: ' + mensaje[0]
-                       
-    else:
-        return
-    
-    hermes.publish_end_session(intent_message.session_id, sentence)
+    intentHandler(hermes, intent_message, mensaje)
+     
 
-# def intent_stop(hermes, intent_message):
-#    if intent_message.intent.intent_name == 'juancarlos:Cancelar':
-#        hermes.publish_end_session(intent_message.session_id, sentence)
+def intent_stop(hermes, intent_message):
+    if intent_message.intent.intent_name == 'juancarlos:Cancelar':
+        hermes.publish_end_session(intent_message.session_id, 'Ya me callo')
  
     
 with Hermes(MQTT_ADDR) as h:
