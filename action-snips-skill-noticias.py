8f2635623dc6968f4391991e627c7a855648443c
JuanCarlosgg <juancarlos97gg@gmail.com>
2019-10-27 12:02:12 +0100

    Pruebas

--- action-snips-skill-noticias.py
+++ action-snips-skill-noticias.py
@@ -43,7 +43,7 @@ def intentHandler(hermes, intent_message):
     N = 3
     titulares = [mensaje[0][n:n+N] for n in range(0, len(mensaje[0]), N)]
     descripcion = [mensaje[1][n:n+N] for n in range(0, len(mensaje[1]), N)]
-    sentence = 'Éstos son los titulares de hoy: ' + titulares[i]
+    sentence = 'Éstos son los titulares de hoy: ' # + titulares[i]
     i = i + 1
     return hermes.publish_continue_session(intent_message.session_id,  sentence, [])
          
@@ -57,7 +57,7 @@ def intent_continuar(hermes, intent_message):
         sentence = titulares[i]
         i = i + 1
         if i >= len(titulares):
-            return hermes.publish_end_session(intent_message.session_id, sentence + '. Esas son todas las noticias')
+            return hermes.publish_end_session(intent_message.session_id, 'hola' + '. Esas son todas las noticias')
         else:    
             return hermes.publish_continue_session(intent_message.session_id,  sentence, [])
         
